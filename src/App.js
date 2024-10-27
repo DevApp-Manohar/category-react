f8a42b7af85a1c4981ee35ffaae89894dfd5edd1
manohark774@gmail.com <manohark774@gmail.com>
2024-10-27 21:27:21 +0530

    Add hashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, Router } from 'react-router-dom';
+import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
 import './App.css';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
@@ -7,14 +7,14 @@ import Login from './pages/Login';
 function App() {
   return (
     <div className="App">
-      <Router basename="/category-react">
+      <HashRouter>
       <Routes>
         <Route path='/' element={<Navigate to="/login" />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/home' element={<Home />} />
       </Routes>
-      </Router>
+      </HashRouter>
       
     </div>
   );
