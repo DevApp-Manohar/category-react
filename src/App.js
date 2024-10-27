f5714f13f2a9cb3cd8297d1a9a77eb1d47f0e567
manohark774@gmail.com <manohark774@gmail.com>
2024-10-27 22:02:13 +0530

    add Browser Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
+import { Navigate, Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
@@ -7,15 +7,15 @@ import Login from './pages/Login';
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter basename="/<repo-name>">
       <Routes>
         <Route path='/' element={<Navigate to="/login" />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/home' element={<Home />} />
       </Routes>
-      </HashRouter>
-      
+      </BrowserRouter>
+    
     </div>
   );
 }
